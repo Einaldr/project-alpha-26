faee23cd99086efc58727cd9130110f405f918ab
Oliwer Czubala <oliwer@czubala.dev>
2026-03-12 11:24:48 +0100

    feat: added mockup capability

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enum\AccountStatus;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -29,12 +30,31 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+
+            'tos_accepted_at' => now(),
+            'tos_version' => config('settings.tos_version', '1.0.0'),
+
+            'account_status' => AccountStatus::ACTIVE,
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
      */
+    public function banned(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'account_status' => AccountStatus::BANNED,
+        ]);
+    }
+
+    public function deleted(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'deleted_at' => now(),
+        ]);
+    }
+
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [

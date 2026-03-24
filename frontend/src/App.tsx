627614f4afdc5c9f76d8ed833b7fdf01b30a227c
Einaldr <einaldr@einaldr.com>
2026-03-24 20:48:40 +0100

    feat(ui): set up axios api handler

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
 import { LoginPage } from "./pages/LoginPage"
+import ProtectedRoute from "./lib/middlewere"
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<h1>Dashboard here</h1>} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/dashboard" element={<h1>Dashboard here</h1>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )

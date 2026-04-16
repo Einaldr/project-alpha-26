761d4f3914be3962779c59c49b1508016a2f6760
Einaldr <einaldr@einaldr.com>
2026-04-16 08:58:21 +0200

    feat(api): create user observer #7
    Created UserObserver to create individual groups at user creation.

---
+++ backend/app/Observers/UserObserver.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Observers;
+
+use App\Enum\GroupType;
+use App\Models\Group;
+use App\Models\User;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     */
+    public function created(User $user): void
+    {
+        $group = Group::create([
+            'name' => $user->name . "'s Workspace",
+            'type' => GroupType::INDIVIDUAL,
+            'owner_id' => $user->id,
+        ]);
+    }
+
+    /**
+     * Handle the User "updated" event.
+     */
+    public function updated(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     */
+    public function deleted(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     */
+    public function restored(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     */
+    public function forceDeleted(User $user): void
+    {
+        //
+    }
+}

3f219d6c58b6a3fd6b2cbd43d148702ca8e83bd4
Oliwer Czubala <oliwer@czubala.dev>
2026-04-29 23:16:21 +0200

    feat(api): declare controller methods #11

--- backend/app/Http/Controllers/Api/GroupMemberController.php
+++ backend/app/Http/Controllers/Api/GroupMemberController.php
@@ -3,9 +3,27 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Group;
+use App\Models\GroupMember;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class GroupMemberController extends Controller
 {
-    //
+    public function groupMembers(Group $group): ResourceCollection
+    {
+
+    }
+
+    public function inviteMember(Group $group, User $user, Request $request): GroupMember
+    {
+
+    }
+
+    public function kickMember(Group $group, User $user, Request $request): JsonResponse
+    {
+        
+    }
 }

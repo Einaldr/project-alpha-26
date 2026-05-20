6522d7d498b0ea78fcb7f59bddb2cef840221bec
Oliwer Czubala <oliwer@czubala.dev>
2026-05-20 22:44:32 +0200

    refactor(frontend): make settings group_type-aware #1

--- frontend/src/components/elements/sidebar-elements/nav-settings.tsx
+++ frontend/src/components/elements/sidebar-elements/nav-settings.tsx
@@ -13,7 +13,11 @@ import {
 } from "@/components/ui/sidebar"
 import { useActiveGroupStore } from "@/hooks/useActiveGroupStore"
 import { useActiveMembership } from "@/hooks/useActiveMembership"
-import { CaretRightIcon, GearIcon } from "@phosphor-icons/react"
+import {
+  CaretDoubleRightIcon,
+  CaretRightIcon,
+  GearIcon,
+} from "@phosphor-icons/react"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
@@ -23,51 +27,66 @@ export default function NavSettings() {
   const navigate = useNavigate()
   const { hasPermission } = useActiveMembership()
 
-  if (!hasPermission('group.update') || !hasPermission('audit_log.view')) return null
+  if (!hasPermission("group.update") || !hasPermission("audit_log.view"))
+    return null
+
+  if (!activeGroup) return null
 
   return (
     <SidebarGroup>
-      <SidebarGroupLabel>Group Settings</SidebarGroupLabel>
-      <Collapsible
-        defaultOpen={false}
-        open={isOpen}
-        onOpenChange={setIsOpen}
-        className="group/collapsible"
-      >
-        <SidebarMenuItem>
-          <CollapsibleTrigger asChild>
-            <SidebarMenuButton
-              className="gap-2"
-              onClick={() => setIsOpen(true)}
-            >
-              <GearIcon className="" />
-              <span>Settings</span>
-              <CaretRightIcon className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
-            </SidebarMenuButton>
-          </CollapsibleTrigger>
-          <CollapsibleContent>
-            <SidebarMenuSub>
-              {hasPermission("group.update") && !(activeGroup?.group_type == 'individual') ? (
-                <SidebarMenuSubButton
-                  asChild
-                  onClick={() => navigate("/group/settings")}
+      {activeGroup.group_type == "individual" ? (
+        <SidebarMenuButton asChild onClick={() => navigate('/group/settings')}>
+          <div>
+            <GearIcon />
+            <span>Settings</span>
+            <CaretDoubleRightIcon className="ml-auto" />
+          </div>
+        </SidebarMenuButton>
+      ) : (
+        <>
+          <SidebarGroupLabel>Group Settings</SidebarGroupLabel>
+          <Collapsible
+            defaultOpen={false}
+            open={isOpen}
+            onOpenChange={setIsOpen}
+            className="group/collapsible"
+          >
+            <SidebarMenuItem>
+              <CollapsibleTrigger asChild>
+                <SidebarMenuButton
+                  className="gap-2"
+                  onClick={() => setIsOpen(true)}
                 >
+                  <GearIcon className="" />
                   <span>Settings</span>
-                </SidebarMenuSubButton>
-              ) : null}
+                  <CaretRightIcon className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                </SidebarMenuButton>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+                <SidebarMenuSub>
+                  {hasPermission("group.update") ? (
+                    <SidebarMenuSubButton
+                      asChild
+                      onClick={() => navigate("/group/settings")}
+                    >
+                      <span>Settings</span>
+                    </SidebarMenuSubButton>
+                  ) : null}
 
-              {hasPermission("audit_log.view") ? (
-                <SidebarMenuSubButton
-                  asChild
-                  onClick={() => navigate("/group/auditlog")}
-                >
-                  <span>Audit Log</span>
-                </SidebarMenuSubButton>
-              ) : null}
-            </SidebarMenuSub>
-          </CollapsibleContent>
-        </SidebarMenuItem>
-      </Collapsible>
+                  {hasPermission("audit_log.view") ? (
+                    <SidebarMenuSubButton
+                      asChild
+                      onClick={() => navigate("/group/auditlog")}
+                    >
+                      <span>Audit Log</span>
+                    </SidebarMenuSubButton>
+                  ) : null}
+                </SidebarMenuSub>
+              </CollapsibleContent>
+            </SidebarMenuItem>
+          </Collapsible>
+        </>
+      )}
     </SidebarGroup>
   )
 }

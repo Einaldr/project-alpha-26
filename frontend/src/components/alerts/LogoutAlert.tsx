f6b6a1bcebb1710fffb0abfb5a137bb2de8b0f3c
Oliwer Czubala <oliwer@czubala.dev>
2026-05-22 23:35:58 +0200

    refactor(frontend): complete header, description and footer #1

--- frontend/src/components/alerts/LogoutAlert.tsx
+++ frontend/src/components/alerts/LogoutAlert.tsx
@@ -1,20 +1,33 @@
 import { SignOutIcon } from "@phosphor-icons/react";
-import { AlertDialog, AlertDialogContent, AlertDialogHeader, AlertDialogTrigger } from "../ui/alert-dialog";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogMedia, AlertDialogTitle, AlertDialogTrigger } from "../ui/alert-dialog";
 import { Button } from "../ui/button";
 
 export default function LogoutAlert() {
     return (
         <AlertDialog>
             <AlertDialogTrigger asChild>
-                <Button variant="destructive">
-                    <SignOutIcon />
+                <Button variant="destructive" className="w-full">
+                    <SignOutIcon className="mr-auto"/>
                     <span>Logout</span>
+                    <SignOutIcon className="ml-auto"/>
                 </Button>
             </AlertDialogTrigger>
             <AlertDialogContent>
                 <AlertDialogHeader>
-                    <AlertDialogHeader></AlertDialogHeader>
+                    <AlertDialogHeader>
+                        <AlertDialogMedia>
+                            <SignOutIcon />
+                        </AlertDialogMedia>
+                        <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                    </AlertDialogHeader>
+                    <AlertDialogDescription>
+                        Are you sure you want to log out?
+                    </AlertDialogDescription>
                 </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Stay</AlertDialogCancel>
+                    <AlertDialogAction>Logout</AlertDialogAction>
+                </AlertDialogFooter>
             </AlertDialogContent>
         </AlertDialog>
     )

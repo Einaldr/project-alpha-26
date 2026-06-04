e80e3d86460f361ab75236b49bdfe1b42829ebfe
Oliwer Czubala <oliwer@czubala.dev>
2026-06-05 00:59:19 +0200

    feat(api): add projects-related tables #29

---
+++ backend/database/migrations/2026_06_04_001737_create_projects_tables.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->foreignUuid('group_id')->constrained()->onDelete('cascade');
+
+            $table->string('name', 64);
+            $table->text('description')->nullable();
+
+            $table->text('icon_url');
+
+            $table->text('git_url');
+            $table->string('default_branch')->default('main');
+
+            $table->timestampTz('last_pulled_at')->nullable();
+            $table->timestampsTz();
+            $table->softDeletes();
+
+            $table->unique(['group_id', 'name']);
+        });
+
+        Schema::create('project_collaborators', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('project_id')->constrained()->onDelete('cascade');
+            $table->foreignUuid('user_id')->constrained()->onDelete('cascade');
+            $table->jsonb('permissions');
+            $table->timestampsTz();
+
+            $table->unique(['project_id', 'user_id']);
+        });
+
+        Schema::create('project_secrets', function (Blueprint $table) {
+            $table->foreignUuid('project_id')->primary()->constrained();
+            $table->string('auth_type')->default('none');
+            $table->text('access_token')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+        Schema::dropIfExists('project_collaborators');
+        Schema::dropIfExists('project_secrets');
+    }
+};
